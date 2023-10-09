6df91c311d5a6b9e2a07b2d350322cfa11bc9fbb
Kevin Pfefferle <kevin@pfefferle.me>
2023-10-09 11:49:51 -0400

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Kevin Pfefferle on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
